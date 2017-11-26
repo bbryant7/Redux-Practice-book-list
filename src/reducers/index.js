073a17aacf5aeed246a17a3a46d6f2e5234c5dd1
Bailey Bryant <bailey.bryant10@gmail.com>
2017-11-26 10:46:25 -0600

    create reducer and action for selecting a specific book

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
 import BookListReducer from './bookListReducer';
+import SelectBookReducer from './selectBookReducer'
 
 const rootReducer = combineReducers({
-  books: BookListReducer
+  books: BookListReducer,
+  selected: SelectBookReducer
+
 });
 
 export default rootReducer;
 
 
 // value for piece of state labeled "books" is what is returned from the BookListReducer function
+// value for piece of state labeled "selected" is what is returned from the SelectedBookReducer function
